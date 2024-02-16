f7d7736a29686f6b5bee583d103f79937514f419
Vitalina Kuzmenko <vitalina.kuzmenko.a@gmail.com>
2024-02-16 10:58:08 +0000

    Create html structure for Header component

--- src/app/components/Header/Header.jsx
+++ src/app/components/Header/Header.jsx
@@ -1,7 +1,43 @@
 import styles from "./Header.module.css";
+import Link from "next/link";
 
 const Header = () => {
-  return <div>Header</div>;
+  return (
+    <header className={styles.header}>
+      <Link href="/" className={styles.logo}>
+        Brainwave.io
+      </Link>
+      <input className={styles.menu_btn} type="checkbox" id="menu-btn" />
+      <label className={styles.menu_icon} for="menu-btn">
+        <span className={styles.navicon}></span>
+      </label>
+      <nav className={styles.menu}>
+        <ul className={styles.nav_links}>
+          <li>
+            <Link href="/demos">Demos</Link>
+          </li>
+          <li>
+            <Link href="/pages">Pages</Link>
+          </li>
+          <li>
+            <Link href="/support">Support</Link>
+          </li>
+          <li>
+            <Link href="/contact">Contact</Link>
+          </li>
+        </ul>
+
+        <div className={styles.nav_buttons}>
+          <Link href="/sign-in">
+            <button className={styles.sign_in_button}>Sign In</button>
+          </Link>
+          <Link href="/sign-up">
+            <button className={styles.sign_in_button}>Sign Up</button>
+          </Link>
+        </div>
+      </nav>
+    </header>
+  );
 };
 
 export default Header;
