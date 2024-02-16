4d1643c68f749a1f5289b0f9a588606d05d02204
Vitalina Kuzmenko <vitalina.kuzmenko.a@gmail.com>
2024-02-16 11:32:24 +0000

    Create Hero component and module.css and add  it to page.jsx

---
+++ src/app/components/Hero/Hero.jsx
@@ -0,0 +1,7 @@
+import styles from "./Hero.module.css";
+
+const Hero = () => {
+  return <section>Hero</section>;
+};
+
+export default Hero;
--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,9 +1,10 @@
+import Hero from "./components/Hero/Hero";
 import styles from "./page.module.css";
 
 export default function Home() {
   return (
     <main>
-      <div>Hello world</div>
+      <Hero />
     </main>
   );
 }
