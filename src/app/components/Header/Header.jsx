b0b5bee3ec25937492773b0fc6322aaa8aa930b1
Vitalina Kuzmenko <vitalina.kuzmenko.a@gmail.com>
2024-02-16 11:12:05 +0000

    Add .header-wrapper class and make max-width for Header

--- src/app/components/Header/Header.jsx
+++ src/app/components/Header/Header.jsx
@@ -4,38 +4,40 @@ import Link from "next/link";
 const Header = () => {
   return (
     <header className={styles.header}>
-      <Link href="/" className={styles.logo}>
-        Brainwave.io
-      </Link>
-      <input className={styles.menu_btn} type="checkbox" id="menu-btn" />
-      <label className={styles.menu_icon} for="menu-btn">
-        <span className={styles.navicon}></span>
-      </label>
-      <nav className={styles.menu}>
-        <ul className={styles.nav_links}>
-          <li>
-            <Link href="/demos">Demos</Link>
-          </li>
-          <li>
-            <Link href="/pages">Pages</Link>
-          </li>
-          <li>
-            <Link href="/support">Support</Link>
-          </li>
-          <li>
-            <Link href="/contact">Contact</Link>
-          </li>
-        </ul>
+      <div className={styles.header_wrapper}>
+        <Link href="/" className={styles.logo}>
+          Brainwave.io
+        </Link>
+        <input className={styles.menu_btn} type="checkbox" id="menu-btn" />
+        <label className={styles.menu_icon} for="menu-btn">
+          <span className={styles.navicon}></span>
+        </label>
+        <nav className={styles.menu}>
+          <ul className={styles.nav_links}>
+            <li>
+              <Link href="/demos">Demos</Link>
+            </li>
+            <li>
+              <Link href="/pages">Pages</Link>
+            </li>
+            <li>
+              <Link href="/support">Support</Link>
+            </li>
+            <li>
+              <Link href="/contact">Contact</Link>
+            </li>
+          </ul>
 
-        <div className={styles.nav_buttons}>
-          <Link href="/sign-in">
-            <button className={styles.sign_in_button}>Sign In</button>
-          </Link>
-          <Link href="/sign-up">
-            <button className={styles.sign_in_button}>Sign Up</button>
-          </Link>
-        </div>
-      </nav>
+          <div className={styles.nav_buttons}>
+            <Link href="/sign-in">
+              <button className={styles.sign_in_button}>Sign In</button>
+            </Link>
+            <Link href="/sign-up">
+              <button className={styles.sign_in_button}>Sign Up</button>
+            </Link>
+          </div>
+        </nav>
+      </div>
     </header>
   );
 };
