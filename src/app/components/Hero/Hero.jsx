e1ace26a8c2c2e0a4c1bbf54dfe0c982ecd33638
Vitalina Kuzmenko <vitalina.kuzmenko.a@gmail.com>
2024-02-16 11:42:57 +0000

    Add css to Hero component, add classes to Hero.jsx

--- src/app/components/Hero/Hero.jsx
+++ src/app/components/Hero/Hero.jsx
@@ -1,7 +1,15 @@
 import styles from "./Hero.module.css";
 
 const Hero = () => {
-  return <section>Hero</section>;
+  return (
+    <section className={styles.hero_section}>
+      <h1 className={styles.hero_title}>Pricing & Plans</h1>
+      <p className={styles.hero_description}>
+        With lots of unique blocks, you can easily build a page without coding.
+        Build your next consultancy website within few minutes.
+      </p>
+    </section>
+  );
 };
 
 export default Hero;
